fc80aa14a4f97851b4365d0f06b391f371393021
unknown <keillon_sil@hotmail.com>
2024-12-23 23:50:38 -0300

    att

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData(){
-    url = 'https://raw.githubusercontent.com/keillon/js-developer-portfolio/refs/heads/main/data/profile.json'
+    url = 'https://raw.githubusercontent.com/keillon/porfolio-js/refs/heads/main/assets/js/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,35 +1,52 @@
-
-
-function updateProfileInfo(profileData){
+function updateProfileInfo (profileData) {
+  setTimeout(() => {
     const name = document.getElementById('profile.name')
     const photo = document.getElementById('profile.photo')
     const location = document.getElementById('profile.location')
     const number = document.getElementById('profile.number')
+    const email = document.getElementById('profile.email')
     name.textContent = profileData.name
     photo.src = profileData.photo
     location.textContent = profileData.location
     number.textContent = profileData.phone
+    email.textContent = profileData.email
+  }, 1500)
 }
 
-(async () =>{
+function updatePortfolio (profileData) {
+  const portfolio = document.getElementById('profile.portfolio')
 
-    const profileData = await fetchProfileData()
-    updateProfileInfo(profileData)
+  portfolio.innerHTML = profileData.portfolio
+    .map(project => {
+      return `
+        <li class="li--bottom">         
+            <img src="assets/img/github.svg" alt="">
+            <a href="${project.url}" target="_blank"><span id="profile.tittle">${project.name}</span></a>
+            <li>
+              <a href="${project.url}" target="_blank"><span id="profile.github">${project.url}</span></a>
+            </li>
+          </li>
+        `
+    })
+    .join('')
+}
 
+function updateLanguages (profileData) {
+  const languages = document.getElementById('profile.languages')
+
+  languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('')
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.soft.skills')
+
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('') 
+}
+
+(async () => {
+  const profileData = await fetchProfileData()
+  updateProfileInfo(profileData)
+  updatePortfolio(profileData)
+  updateLanguages(profileData)
+  updateSoftSkills(profileData)
 })()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
